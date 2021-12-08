a68918bf6f015fc17a6e67452f03cb842b4f6b61
barberrybeats <ramis040404@gmail.com>
2021-12-08 15:58:42 +0600

    Thirst global update. I added a design.

--- app/src/main/java/kg/geektech/lvl4lesson1/MainActivity.java
+++ app/src/main/java/kg/geektech/lvl4lesson1/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
