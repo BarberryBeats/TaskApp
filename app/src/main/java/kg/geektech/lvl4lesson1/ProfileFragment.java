0bbe18c3654cd326df558c9b00ee647696fe69dc
barberrybeats <ramis040404@gmail.com>
2021-12-08 12:44:51 +0600

    Second update1

--- app/src/main/java/kg/geektech/lvl4lesson1/ProfileFragment.java
+++ app/src/main/java/kg/geektech/lvl4lesson1/ProfileFragment.java
@@ -71,6 +71,7 @@ public class ProfileFragment extends Fragment {
                 e.printStackTrace();
             }
             binding.roundImageViewBody.setImageBitmap(image);
+            /**/
         }
     }
 }
